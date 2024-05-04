bbd193f8173f85a1cc2571175cb808e96ce8d788
jtlewis81 <jtlewis91481@gmail.com>
2024-05-04 17:37:04 -0400

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -5,14 +5,14 @@ error_reporting(E_ALL);
 
 // modify these settings according to the account on your database server.
 //$host = "localhost";
-$host = "cst407.mysql.database.azure.com";
-$port = "3306";
+$host = 'cst407.mysql.database.azure.com';
+$port = '3306';
 //$username = "root";
-$username = "ymappqobfm";
+$username = 'ymappqobfm';
 //$user_pass = "root";
-$user_pass = "jpq5Gkkt9oyxP1\$V";
+$user_pass = 'jpq5Gkkt9oyxP1$V';
 //$database_in_use = "jokesdb";
-$database_in_use = "cst407-jokes-app-database";
+$database_in_use = 'cst407-jokes-app-database';
 
 $mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
 

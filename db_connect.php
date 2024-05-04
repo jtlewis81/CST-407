e19a26feeee03b2dfdf6d10639899390e2c8d6f3
jtlewis81 <jtlewis91481@gmail.com>
2024-05-04 10:08:44 -0400

    mysqli fix attempt 3

--- db_connect.php
+++ db_connect.php
@@ -5,20 +5,24 @@ error_reporting(E_ALL);
 
 // modify these settings according to the account on your database server.
 //$host = "localhost";
-//$port = "3306";
+$host = "cst407.mysql.database.azure.com";
+$port = "3306";
 //$username = "root";
+$username = "ymappqobfm";
 //$user_pass = "root";
+$user_pass = "jpq5Gkkt9oyxP1\$V";
 //$database_in_use = "jokesdb";
+$database_in_use = "cst407-jokes-app-database";
 
-$con = mysqli_init();
-//mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
-mysqli_ssl_set($con,NULL,NULL, NULL, NULL, NULL);
-$mysqli = mysqli_real_connect($con, "cst407.mysql.database.azure.com", "ymappqobfm", "jpq5Gkkt9oyxP1\$V", "cst407-jokes-app-database", 3306, MYSQLI_CLIENT_SSL);
+$mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
+//$con = mysqli_init();
+//mysqli_ssl_set($con,NULL,NULL, NULL, NULL, NULL);
+//$mysqli = mysqli_real_connect($con, "cst407.mysql.database.azure.com", "ymappqobfm", "jpq5Gkkt9oyxP1\$V", "cst407-jokes-app-database", 3306, MYSQLI_CLIENT_SSL);
 
 // $mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
-// if ($mysqli->connect_error) {
-//     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-// }
-// echo $mysqli->host_info . "<br>";
+if ($mysqli->connect_error) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+echo $mysqli->host_info . "<br>";
 
 ?>
